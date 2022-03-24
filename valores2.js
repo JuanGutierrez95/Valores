10c3c8dc3b7763813299ac5d75475ba1906659ac
ale9516 <juan.gutierrez19958@gmail.com>
2022-03-24 14:14:49 -0300

    first commit

---
+++ valores2.js
@@ -0,0 +1,43 @@
+40 //Numero
+
+"Juan" //String : Cadena de texto que vamos a utilizar para generar un valor que nosotros podemos utilizarlo o llamar despues para que el usuario pueda leer o pueda generar como algun tipo de interaccion.
+//Nosotros queremos guardar texto, frases, palabras lo vamos a hacer con un string y esto nosotros podemos utilizarlo despues.
+
+//Tenemos otro tipo de valor
+//Valor Booleano
+true
+false
+//Lo genero dentro del lenguaje de programacion que nosotros podamos validar cosas. Ocupamos que el true o false para poder validar si tomamos una decision o no.
+//Y esto es el tipo de valor que vamos a utilizar.
+
+
+//Existen otros dos tipos de valores que se le llaman valores vacios 
+null
+undefined
+//Estos son dos valores se lo conoce como valores pro.
+//Son valores reservados para algun tipo de valor.
+//Cuando queda algun valor faltante, nosotros tenemos que guardar un valor en memoria. Cuando todavia no tenemos ese valor. Podemos ocupar un nulo o undefined para poder generar este espacio reservado para despues poder colocar un valor, pero tambien JavaScript en algun momento, si nosotros estamos utilizando valores o no utilizando valores en ciertas partes del codigo, nos pueden salir estos dos valores que resulte como error.
+//Es importante tener mucho cuidado en esto.
+ //Undefined es importante evitarlo porque es algo, es un valor como reservado que genera JavaScript 
+
+ //A estos tipos de valores se le conoce como valores primitivos, porque son los valores basicos que nosotros vamos a utilizar para poder generar codigo.
+
+
+ //Tambien existen dos tipos de valores mas, que son valores no primitivos o valores tipos objetos. El primero seria el valor de array o tipo array.
+ //Este valor lo vamos a generar ocupando corchetes y adentro podemos, poner algo de informacion casi siempre son valores primitivos, adentro de estos corchetes, generan un array y esto es un tipo objeto.[]
+
+ [1,2,3]
+
+ //Y tenemos otro que es el valor tipo Objeto, que lo vamos a hacer con corchetes {}.
+
+ {nombre: "Juan"}
+
+
+ typeof 40; //"number"
+ typeof "Juan" // "string"
+ typeof true // "boolean"
+ typeof false //"boolean"
+ typeof null //"object"
+ typeof undefined//"undefined"
+typeof [] //objeto
+typeof {} //objeto
